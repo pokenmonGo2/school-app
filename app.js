eb7afb7eff7fa442f48325eeb0fef42a09fb71a4
pokenmonGo2 <jorncrombag78@gmail.com>
2026-09-07 10:44:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+function showPage(pageName) {
+    const pages = document.querySelectorAll(".page");
 
+    pages.forEach(page => {
+        page.classList.remove("active");
+    });
+
+    document.getElementById(pageName).classList.add("active");
+}
+
+const today = new Date();
+
+document.getElementById("today").textContent =
+    today.toLocaleDateString("nl-NL", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
